a6be992ca7dc7adcce456d53df85983d0dcdaa1a
Galkin Alexey Andreevich <97546730+GalkinAlexeyAndreevich@users.noreply.github.com>
2023-08-08 20:06:21 +0300

    Роутинг,верстка авторизации

---
+++ online-shop/client/src/routes/index.js
@@ -0,0 +1,40 @@
+import Admin from "../pages/AdminPage";
+import Basket from "../pages/BasketPage";
+import Auth from "../pages/AuthPage";
+import Shop from "../pages/ShopPage";
+import Device from "../pages/DevicePage";
+import { ADMIN_ROUTE, BASKET_ROUTE, DEVICE_ROUTE, LOGIN_ROUTE, REGISTRATION_ROUTE, SHOP_ROUTE } from "../utils/consts";
+
+export const authRoutes = [
+	{
+		Component: <Admin/>,
+		path: ADMIN_ROUTE,
+	},
+    {
+		Component: <Basket/>,
+		path: BASKET_ROUTE,
+	},
+];
+
+export const publicRoutes = [
+	{
+		Component: <Shop/>,
+		path: SHOP_ROUTE,
+	},
+    {
+		Component: <Auth/>,
+		path: LOGIN_ROUTE,
+	},
+    {
+		Component: <Auth/>,
+		path: REGISTRATION_ROUTE,
+	},
+    {
+		Component: <Device/>,
+		path: DEVICE_ROUTE + '/:id',
+	},
+	{
+		Component: <Shop/>,
+		path: '*',
+	},
+];
